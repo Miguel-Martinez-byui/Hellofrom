865cf386270a8e259b74b1120bed0db8cfa39108
Yaswell-BYU <78278752+Yaswell-BYU@users.noreply.github.com>
2021-01-31 18:19:54 -0400

    Add files via upload

---
+++ HelloFromYaswell.java
@@ -0,0 +1,9 @@
+public class HelloFromYaswell {
+    public static void main(String[] args) {
+        new HelloFromYaswell();
+
+    }
+    public HelloFromYaswell() {
+           System.out.println("Hello from Yaswell");
+    }
+}
